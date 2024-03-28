1cb7ca2bb134b87292ff9f8e96f17656fe4fcfa2
Vincent7k <nicckamorim@gmail.com>
2024-03-28 11:47:35 -0300

    still var

---
+++ escopo.js
@@ -0,0 +1,19 @@
+if (true) {
+  var cat = "Maneki Neko";
+//   console.log(cat);
+}
+
+cat = "Dwarf";
+//console.log(cat);
+
+// ctrl : comenta
+
+if (1 > 0) {
+    let dog = 'caramel';
+    console.log(dog);
+
+    dog = 'Bull';
+    // vai implicito
+    console.log(dog);
+
+}
