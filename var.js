dc5583080b2ce44f1c2a691e87430cbb968162ae
Vincent7k <nicckamorim@gmail.com>
2024-03-28 11:33:17 -0300

    chore:exercises with var

---
+++ var.js
@@ -0,0 +1,13 @@
+let pet = "Cat";
+pet = 'Rajadinho';
+
+var dog ='Caramel';
+littleDog = 'Small Dog';
+
+let professor = 'Thiago';
+
+const petAge = 1; 
+//String = texto puro
+
+console.log(`My pet chose is: ${pet} \n his age is: ${petAge} age`);
+//console.log(pet);
